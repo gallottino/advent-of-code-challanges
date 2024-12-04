ba17d798d9421c92c91b2cdb6ec150dfa3f687b8
gallottino <d.gallotti@outlook.it>
2024-12-04 01:10:35 +0100

    day 3 part 2

--- 2024/day3/day3.py
+++ 2024/day3/day3.py
@@ -15,21 +15,15 @@ def get_number(tokens, start_idx, end_char) -> list[int, int]:
     return [number, current_idx]
 
 
-def mul_pattern(tokens, start_idx, pattern = "mul(") -> list[int, int]:
+def mul_pattern(tokens, start_idx) -> list[int, int]:
     current_idx = start_idx
-    pattern_list = list(pattern)
 
-    matched = True
-    while current_idx < len(tokens) and len(pattern_list) > 0 and matched:
-        if tokens[current_idx] != pattern_list[0]:
-            matched = False
-        pattern_list.pop(0)
-        current_idx += 1
+    matched = match_pattern(tokens, start_idx, pattern="mul(")
 
     if not matched:
         return [0, current_idx]
 
-    [first_number, current_idx] = get_number(tokens, current_idx, ',')
+    [first_number, current_idx] = get_number(tokens, current_idx + 4, ',')
     if first_number < 0 :
         return [0, current_idx]
 
@@ -38,9 +32,35 @@ def mul_pattern(tokens, start_idx, pattern = "mul(") -> list[int, int]:
         return [0, current_idx]
 
     return [first_number * second_number, current_idx]
-    
 
-def solve(tokens) -> int:
+# I don't want to use regular expression
+def match_pattern(tokens, start_idx, pattern="") -> bool:
+    current_idx = start_idx
+    pattern_list = list(pattern)
+    while current_idx < len(tokens) and len(pattern_list) > 0 :
+        if tokens[current_idx] != pattern_list[0]:
+            return False
+        pattern_list.pop(0)
+        current_idx += 1
+    return True
+
+def solve_part2(tokens) -> int:
+    idx = 0
+    total_amount = 0
+    do = True
+    while idx < len(tokens):
+        if tokens[idx] == 'd':
+            if match_pattern(tokens, idx, pattern="don't()"):
+                do = False
+            elif match_pattern(tokens, idx, pattern="do()"):
+                do = True
+        if do and tokens[idx] == 'm':
+            [amount, idx] = mul_pattern(tokens, idx)
+            total_amount += amount
+        idx += 1
+    return total_amount
+
+def solve_part1(tokens) -> int:
     idx = 0
     total_amount = 0
     while idx < len(tokens):
@@ -54,8 +74,10 @@ def main() -> None:
     
     with open("input.txt") as file:
         data = file.read()
-        result = solve(list(data))
-        print(result)
+        result_part1 = solve_part1(list(data))
+        result_part2 = solve_part2(list(data))
+        print("Part 1: ",result_part1)
+        print("Part 2: ",result_part2)
 
 if __name__ == "__main__":
     main()
