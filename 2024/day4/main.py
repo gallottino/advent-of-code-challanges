7312d3569b61aeb32e72a853d295462bdf88c7f2
gallottino <d.gallotti@outlook.it>
2024-12-04 15:44:47 +0100

    day4 part 2

--- 2024/day4/main.py
+++ 2024/day4/main.py
@@ -13,7 +13,7 @@ def out_of_bound(map: list[list[str]], row_index, col_index):
 def is_pattern(
     map: list[list[str]], 
     row_index, col_index,
-    offset: list[int,int],
+    direction: list[int,int],
     pattern: list[str]
 ) -> int:
     if len(pattern) <= 0:
@@ -25,17 +25,21 @@ def is_pattern(
     if map[row_index][col_index] != pattern[0]:
         return 0
 
-    [row_off, col_off] = offset
+    [row_off, col_off] = direction
     return is_pattern(
         map,
         row_index + row_off,
         col_index + col_off,
-        offset,
+        direction,
         pattern=pattern[1:]
     )
 
-def get_pattern_occurrencies(map, row_index, col_index, pattern) -> int:
-    offsets=[
+def get_pattern_occurrencies(
+    map,
+    row_index,
+    col_index,
+    pattern) -> int:
+    offsets = [
         [-1,-1],
         [-1, 0],
         [-1, 1],
@@ -55,7 +59,7 @@ def get_pattern_occurrencies(map, row_index, col_index, pattern) -> int:
         )
     return occurrencies
 
-def solve(map: list[list[str]]) -> int:
+def solve_part1(map: list[list[str]]) -> int:
     total = 0
 
     pattern = ["X", "M", "A", "S"]
@@ -66,11 +70,53 @@ def solve(map: list[list[str]]) -> int:
 
     return total
 
+def solve_part2(map: list[list[str]]) -> int:
+    total = 0
+
+    directions_first_type = [
+        [[0,0], [1,  1]],
+        [[0,2], [1, -1]],
+        [[2,0], [-1, 1]],
+        [[2,2], [-1, -1]],
+    ]
+
+    directions_second_type = [
+        [[0,0],   [-1,-1]],
+        [[0,-2],  [-1, 1]],
+        [[-2,0],  [ 1,-1]],
+        [[-2,-2], [ 1, 1]],
+    ]
+
+    total = 0
+    pattern = ["M", "A", "S"]
+    for row_index, row in enumerate(map):
+        for col_index, value in enumerate(row):
+            if value == pattern[0]:
+                matched_first_type = [
+                    is_pattern(map, row_index + row_off, col_index + col_off, direction, pattern)
+                    for [row_off, col_off], direction in directions_first_type
+                ]
+                if sum(matched_first_type) > 1:
+                    total += 1
+
+                matched_second_type = [
+                    is_pattern(map, row_index + row_off, col_index + col_off, direction, pattern)
+                    for [row_off, col_off], direction in directions_second_type
+                ]
+                if sum(matched_second_type) > 1:
+                    total += 1
+
+    return total
+
 def main():
     with open("input.txt") as file:
         data = file.read().splitlines()
-        result_1 = solve([list(line) for line in data])
+
+        result_1 = solve_part1([list(line) for line in data])
         print("Part 1: ", result_1)
+
+        result_2 = solve_part2([list(line) for line in data])
+        print("Part 2: ", result_2)
 
 if __name__ == "__main__":
     main()
