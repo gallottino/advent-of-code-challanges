3bfdf84a7169b3f8392c515f5075842f4622fc53
gallottino <d.gallotti@outlook.it>
2024-12-05 14:01:28 +0100

    day 5 part 1

---
+++ 2024/day5/main.py
@@ -0,0 +1,47 @@
+
+def get_rules_set(rules: list[str]):
+    rules_set = {}
+    for rule in rules:
+        [prev, next] = rule.split("|")
+        next_list = rules_set[prev] if prev in rules_set.keys() else []
+        next_list.append(next)
+        rules_set[prev] = next_list[:]
+
+    return rules_set
+
+def check_line(line: list[str], rules_set: {int,list[int]}) -> int:
+    prev_set = set()
+    for page in line:
+        if page in rules_set.keys():
+            if not check_rule(prev_set, rules_set[page]):
+                    return 0
+        prev_set.add(page)
+                
+    return line[int(len(line)/2)]
+
+def check_rule(prev_set, rule: list[int]):
+    for value in rule:
+        if value in prev_set:
+            return False
+
+    return True
+
+def solve_part1(lines: list[list[str]], rules_set: {int,list[int]}) -> int:
+    total = 0
+    for line in lines:
+        middle_value = check_line(line.split(","), rules_set)
+        total += int(middle_value)
+    return total
+
+def main() -> None:
+    with open("input.txt") as file:
+        data = file.read().splitlines()
+        end_rules_idx = data.index("")
+        rules = data[0:end_rules_idx]
+        rules_set = get_rules_set(rules)
+
+        lines = data[end_rules_idx+1:]
+        print("Part 1: ", solve_part1(lines, rules_set))
+
+if __name__ == "__main__":
+    main()
