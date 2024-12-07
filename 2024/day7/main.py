84758f070e78850121ffb51150c82e6c3f5d82ce
Davide Gallotti <d.gallotti@outlook.it>
2024-12-07 20:04:44 +0100

    day 7 part 2

--- 2024/day7/main.py
+++ 2024/day7/main.py
@@ -10,16 +10,42 @@ def is_valid(amount: int, current:int, operands: list) -> int:
 
     return 0
 
-def main() -> None:
+def is_valid_with_concat(amount: int, current:int, operands: list) -> int:
+    if len(operands) == 0:
+        return amount if current == amount else 0
+
+    if is_valid_with_concat(amount, current + operands[0], operands[1:]):
+        return amount
+    if is_valid_with_concat(amount, current * operands[0], operands[1:]):
+        return amount
+    if is_valid_with_concat(amount, int(str(current) + str(operands[0])), operands[1:]):
+        return amount
+
+    return 0
+
+def solve_part2(data: list[str]) -> int:
     total = 0
+    for line in data:
+        [amount, operands] = line.split(":")
+        amount = int(amount)
+        operands =  [int(operand) for operand in operands.split(" ")[1:]]
+        total += is_valid_with_concat(amount, 0, operands)
+    return total
+
+def solve_part1(data: list[str]) -> int:
+    total = 0
+    for line in data:
+        [amount, operands] = line.split(":")
+        amount = int(amount)
+        operands =  [int(operand) for operand in operands.split(" ")[1:]]
+        total += is_valid(amount, 0, operands)
+    return total
+
+def main() -> None:
     with open("input.txt") as file:
         data = file.read().splitlines()
-        for line in data:
-            [amount, operands] = line.split(":")
-            amount = int(amount)
-            operands =  [int(operand) for operand in operands.split(" ")[1:]]
-            total += is_valid(amount, 0, operands)
-
-    print("Part 1:", total)
+        print("Part 1:", solve_part1(data))
+        print("Part 2:", solve_part2(data))
+    
 if __name__ == "__main__":
     main()
