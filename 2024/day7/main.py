308bf6b1433204b274961d938fd51d6b518d04fd
Davide Gallotti <d.gallotti@outlook.it>
2024-12-07 20:14:26 +0100

    clean code

--- 2024/day7/main.py
+++ 2024/day7/main.py
@@ -1,51 +1,32 @@
-
-def is_valid(amount: int, current:int, operands: list) -> int:
+def is_valid(amount: int, current:int, operands: list, operations: dict[str, any]) -> int:
+    
     if len(operands) == 0:
         return amount if current == amount else 0
 
-    if is_valid(amount, current + operands[0], operands[1:]):
-        return amount
-    if is_valid(amount, current * operands[0], operands[1:]):
-        return amount
+    for operation in operations:
+        if is_valid(amount, operations[operation](current, operands[0]), operands[1:], operations):
+            return amount
 
     return 0
-
-def is_valid_with_concat(amount: int, current:int, operands: list) -> int:
-    if len(operands) == 0:
-        return amount if current == amount else 0
-
-    if is_valid_with_concat(amount, current + operands[0], operands[1:]):
-        return amount
-    if is_valid_with_concat(amount, current * operands[0], operands[1:]):
-        return amount
-    if is_valid_with_concat(amount, int(str(current) + str(operands[0])), operands[1:]):
-        return amount
-
-    return 0
-
-def solve_part2(data: list[str]) -> int:
-    total = 0
-    for line in data:
-        [amount, operands] = line.split(":")
-        amount = int(amount)
-        operands =  [int(operand) for operand in operands.split(" ")[1:]]
-        total += is_valid_with_concat(amount, 0, operands)
-    return total
-
-def solve_part1(data: list[str]) -> int:
-    total = 0
-    for line in data:
-        [amount, operands] = line.split(":")
-        amount = int(amount)
-        operands =  [int(operand) for operand in operands.split(" ")[1:]]
-        total += is_valid(amount, 0, operands)
-    return total
 
 def main() -> None:
+    amount_part1 = 0
+    amount_part2 = 0
+    operations = {
+        "+": lambda x, y: x + y,
+        "*": lambda x, y: x * y,
+    }
+    operations_with_concat = {**operations, "||": lambda x, y: int(str(x) + str(y))}
     with open("input.txt") as file:
         data = file.read().splitlines()
-        print("Part 1:", solve_part1(data))
-        print("Part 2:", solve_part2(data))
+        for line in data:
+            [amount, operands] = line.split(":")
+            amount = int(amount)
+            operands =  [int(operand) for operand in operands.split(" ")[1:]]
+            amount_part1 += is_valid(amount, 0, operands, operations)
+            amount_part2 += is_valid(amount, 0, operands, operations_with_concat)
+        print("Part 1:", amount_part1)
+        print("Part 2:", amount_part2)
     
 if __name__ == "__main__":
     main()
